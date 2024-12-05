5939635b0c46ee9d7443c063698c23e60601f469
Deivith Zanella <correoserio29112002@gmail.com>
2024-12-04 22:59:16 -0400

    Implementacion de Filum Porifera y Cnidaria

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from acciones import engine
 
 
 def main():
-    engine.base.from_json("Base_De_Conocimiento/Filum.json")  # Por defecto
+    engine.base.from_json("Base_De_Conocimiento/Filumcopy.json")  # Por defecto
     app = menu.Interfaz()
     app.mainloop()
 
